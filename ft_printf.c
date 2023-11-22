a8294c779b3eb75cf39983c8a57f576d7788ca94
Simon <svan-hoo@student.codam>
2023-11-22 20:07:08 +0100

    %, c, s, id, x, X working, no err handling

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:13:52 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/11/22 18:36:08 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/11/22 20:05:12 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,27 @@
 
 int	ft_cprint(char c)
 {
-	ft_putchar(c);
-	return (1);
+	return (ft_putchar(c));
 }
 
 int	ft_sprint(char *str)
 {
-	ft_putstr(str);
-	return (ft_strlen(str));
+	return (ft_putstr(str));
 }
 
-int	ft_idprint(int arg)
+int	ft_idprint(int id)
 {
-	ft_putnbr(arg);
-	return (ft_intlen(arg));
+	return (ft_putnbr(id));
+}
+
+int	ft_xprint(int x)
+{
+	return (ft_putnbr_base(x, "0123456789abcdef"));
+}
+
+int	ft_upxprint(int x)
+{
+	return (ft_putnbr_base(x, "0123456789ABCDEF"));
 }
 
 int	ft_spellbook(va_list args, const char *form)
@@ -36,11 +43,15 @@ int	ft_spellbook(va_list args, const char *form)
 	if (*form == '%')
 		return (ft_cprint('%'));
 	if (*form == 'c')
-		return (ft_cprint(va_arg(args, char)));
+		return (ft_cprint(va_arg(args, int)));
 	if (*form == 's')
 		return (ft_sprint(va_arg(args, char *)));
 	if (*form == 'i' || *form == 'd')
 		return (ft_idprint(va_arg(args, int)));
+	if (*form == 'x')
+		return (ft_xprint(va_arg(args, int)));
+	if (*form == 'X')
+		return (ft_upxprint(va_arg(args, int)));
 	return (0);
 }
 
@@ -51,13 +62,14 @@ int	ft_printf(const char *form, ...)
 	int		i;
 
 	va_start(args, form);
+	bytes_printed = 0;
 	i = 0;
 	while (form[i])
 	{
 		if (form[i] == '%')
 			bytes_printed += ft_spellbook(args, (form + ++i));
 		else
-			bytes_printed += write(1, form, 1);
+			bytes_printed += write(1, (form + i), 1);
 		i++;
 	}
 	va_end(args);
@@ -66,9 +78,6 @@ int	ft_printf(const char *form, ...)
 
 int	main(void)
 {
-	ft_printf("%%\n", 5);
-	ft_printf("%c\n", 5);
-	ft_printf("%s\n", 5);
-	ft_printf("%i\n", 5);
+	ft_printf("%%\n%c\n%s\n%i\n%d\n%x\n%X\n", 'c', "string", 12345, 12345, 164, 164);
 	return (0);
 }

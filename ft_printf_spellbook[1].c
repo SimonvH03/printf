1db45426652fedf57d6f0befd5ba6210f3e427ca
Simon <svan-hoo@student.codam>
2023-11-24 18:08:50 +0100

    all OK, not strict, no bonus

--- ft_printf_spellbook[1].c
+++ ft_printf_spellbook[1].c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_spellbook[1].c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 21:11:55 by simon             #+#    #+#             */
-/*   Updated: 2023/11/24 00:27:01 by simon            ###   ########.fr       */
+/*   Updated: 2023/11/24 18:01:24 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ int	ft_cprint(char c)
 	return (ft_putchar(c));
 }
 
-int	ft_sprint(char *str)
-{
-	return (ft_putstr(str));
-}
-
 int	ft_idprint(int id)
 {
 	return (ft_putnbr(id));
@@ -30,4 +25,14 @@ int	ft_idprint(int id)
 int	ft_uprint(unsigned int u)
 {
 	return (ft_putnbr(u));
+}
+
+int	ft_xprint(int x)
+{
+	return (ft_putnbr_base(x, "0123456789abcdef"));
+}
+
+int	ft_bigxprint(int x)
+{
+	return (ft_putnbr_base(x, "0123456789ABCDEF"));
 }
--- ft_printf_spellbook[2].c
+++ ft_printf_spellbook[2].c
@@ -3,36 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_spellbook[2].c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 21:11:55 by simon             #+#    #+#             */
-/*   Updated: 2023/11/24 00:26:53 by simon            ###   ########.fr       */
+/*   Updated: 2023/11/24 18:08:04 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_xprint(int x)
+int	ft_sprint(char *str)
 {
-	return (ft_putnbr_base(x, "0123456789abcdef"));
-}
-
-int	ft_bigxprint(int x)
-{
-	return (ft_putnbr_base(x, "0123456789ABCDEF"));
+	if (str == NULL)
+		return (write(1, "(null)", 5));
+	else
+		return (ft_putstr(str));
 }
 
 int	ft_pprint(void *p)
 {
-	int	len;
-
-	len = 0;
 	if (p == NULL)
-		len += write(1, "(nil)", 5);
+		return (write(1, "(nil)", 5));
 	else
-	{
-		len += write(1, "0x", 2);
-		len += ft_putpointer((unsigned long)p);
-	}
-	return (len);
+		return (write(1, "0x", 2) + ft_putpointer((unsigned long)p));
 }
